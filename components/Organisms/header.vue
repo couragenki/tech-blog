d8be07dcb2974f33b2be59d2c04eaf9a688ee20d
genki.matsubara <genki.meado@gmail.com>
2020-08-09 12:10:11 +0900

    set up nuxt

---
+++ components/Organisms/header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+    <div>ブログタイトル</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  height: 100px;
+  background: orange;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header />
+    <h2>TOP PAGE</h2>
+    <!-- static -->
+    <img src="/img/sample.jpg" />
+    <!-- assets -->
+    <img src="~/assets/img/sample.jpg" />
+  </div>
+</template>
+
+<script lang="ts">
+import Header from "@/components/Organisms/header.vue";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
