9c0e40f05a3d7ed57aec155d905634990fb8ad28
genki.matsubara <genki.meado@gmail.com>
2020-08-09 22:53:14 +0900

    孫ページを追加

---
+++ pages/tech/backend/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-for="n in backend" :key="n.slug">
+      <nuxt-link :to="'/tech/backend/'+ n.slug">{{n.title}} {{n.date}}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  async asyncData({ $content }: { $content: any }) {
+    const query = await $content("tech/backend" || "index").limit(15);
+    const backend = await query.fetch();
+    return { backend };
+  },
+};
+</script>
---
+++ pages/tech/frontend/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-for="n in frontend" :key="n.slug">
+      <nuxt-link :to="'/tech/frontend/'+ n.slug">{{n.title}} {{n.date}}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  async asyncData({ $content }: { $content: any }) {
+    const query = await $content("tech/frontend" || "index").limit(15);
+    const frontend = await query.fetch();
+    return { frontend };
+  },
+};
+</script>
