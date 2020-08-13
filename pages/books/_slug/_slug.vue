c2ac9dd07ead152bb1b47926d8a65d1cca10e27f
genki.matsubara <genki.meado@gmail.com>
2020-08-13 23:49:19 +0900

    add page and md

---
+++ pages/books/_slug/_slug.vue
@@ -0,0 +1,25 @@
+<template>
+  <article>
+    <h1>{{books.title}}</h1>
+    <dl>
+      <dt>date</dt>
+      <dd>{{books.date}}</dd>
+    </dl>
+    <div>
+      <img :src="books.image" />
+    </div>
+    <div v-for="tag in books.tags" :key="tag">{{tag}}</div>
+
+    <nuxt-content :document="books" />
+  </article>
+</template>
+
+<script lang="ts">
+export default {
+  //@ts-ignore
+  async asyncData({ $content, params }) {
+    const books = await $content("books", params.slug || "index").fetch();
+    return { books };
+  },
+};
+</script>
---
+++ pages/books/_slug/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-for="n in books" :key="n.slug">
+      <nuxt-link :to="'/books/'+ n.slug">{{n.title}} {{n.date}}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  //@ts-ignore
+  async asyncData({ $content }: { $content: any }) {
+    const query = await $content("books" || "index").limit(15);
+    const books = await query.fetch();
+    return { books };
+  },
+};
+</script>
