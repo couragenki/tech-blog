79955768ea31cd480222a4dfc71efdb9730dd82d
genki.matsubara <genki.meado@gmail.com>
2020-08-16 01:35:31 +0900

    fix meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,8 @@ export default {
     title: 'genki-blog',
     meta: [
       { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Meta description' }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
