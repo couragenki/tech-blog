e1b6496339c9291cd6a033b20228433d9eca29e5
genki.matsubara <genki.meado@gmail.com>
2020-08-16 22:23:32 +0900

    add style

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,6 +9,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+*{
+  font-family: "游ゴシック Medium", "Yu Gothic Medium", "游ゴシック体", "YuGothic", "ヒラギノ角ゴ ProN W3", "Hiragino Kaku Gothic ProN", "メイリオ", "Meiryo", "verdana", sans-serif;
+}
 body,
 p,ul,ol,h1,h2,h3,h4,h5,h6 {
   margin: 0;
